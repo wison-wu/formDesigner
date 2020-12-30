910c15fe950d22eff950abd47fdfed6d3bd45b6d
wurong19870715@sohu.com <81777281gitee>
2020-12-31 00:26:29 +0800

    封装富文本编辑器

---
+++ src/components/extend/fancyEditor.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="edit_container">
+        <quill-editor 
+            v-model="content" 
+            ref="myQuillEditor" 
+            :options="editorOption" 
+            @blur="onEditorBlur($event)" @focus="onEditorFocus($event)"
+            @change="onEditorChange($event)">
+        </quill-editor>
+    </div>
+</template>
+
+<script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import { quillEditor } from 'vue-quill-editor'
+export default {
+    components:{
+        quillEditor
+    },
+    data() {
+        return {
+            content: '富文本编辑器',
+            editorOption: {}
+        }
+    },
+    methods: {
+        onEditorReady(editor) { // 准备编辑器
+
+        },
+        onEditorBlur(){}, // 失去焦点事件
+        onEditorFocus(){}, // 获得焦点事件
+        onEditorChange(){}, // 内容改变事件
+    },
+    computed: {
+        editor() {
+            return this.$refs.myQuillEditor.quill;
+        },
+    }
+
+}
+</script>
+
+<style>
+
+</style>
