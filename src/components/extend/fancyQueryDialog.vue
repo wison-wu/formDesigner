05288ae80f7438d2f4a6ebb3d04418b1aa026274
wurong19870715@sohu.com <81777281gitee>
2021-02-28 23:37:41 +0800

    增加dialogQuery组件

---
+++ src/components/extend/fancyQueryDialog.vue
@@ -0,0 +1,23 @@
+<!--文本扩展-->
+<template>
+  <div >
+  </div>
+  
+</template>
+
+<script>
+export default {
+    name:"fancyQueryDialog",
+    props:{
+        value: { 
+            type: String,
+            default:''
+        }
+    },
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
