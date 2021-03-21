05bd598ffdd9f57bdff2c5407ef15d7c46758b24
wison <wurong715@163.com>
2021-03-21 17:02:03 +0800

    集成扩展组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,15 @@
-import formDesigner from '@/components/formDesigner'
+import formDesigner from '@/components/formDesigner';
+import fancyEditor from '@/components/extend/fancyEditor';
+import fancyHtml from '@/components/extend/fancyHtml';
+import fancyText from '@/components/extend/fancyText';
+import fancyQueryDialog from '@/components/extend/fancyQueryDialog';
 const plugins = {
   install : function (Vue) {
     Vue.component('form-designer',formDesigner);
+    Vue.component('fancy-editor',fancyEditor);
+    Vue.component('fancy-html',fancyHtml);
+    Vue.component('fancy-text',fancyText);
+    Vue.component('fancy-query-dialog',fancyQueryDialog);
   }
 };
 // 这一步判断window.Vue是否存在，因为直接引用vue.min.js， 它会把Vue绑到Window上，我们直接引用打包好的js才能正常跑起来。
